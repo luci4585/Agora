1fadfde65984570de685e237a1e9065f24d3a42f
luci4585 <lucilencina6@gmail.com>
2025-09-03 18:07:08 -0300

    Añadir autenticación con Firebase y página de inicio de sesión
    Se ha implementado una nueva página de inicio de sesión en `Login.razor` con campos para correo electrónico y contraseña, y botones para acceder y registrarse. Se ha integrado el servicio `FirebaseAuthService` para manejar la autenticación y se ha añadido `SweetAlertService` para mostrar errores.
    
    Además, se han actualizado `Program.cs` y `_Imports.razor` para incluir las referencias necesarias, y se ha añadido el paquete `CurrieTechnologies.Razor.SweetAlert2` en `WebBlazor.csproj`. También se han incorporado los scripts de Firebase en `index.html` y se ha creado el servicio `FirebaseAuthService.cs` para gestionar la autenticación de usuarios.

--- WebBlazor/Program.cs
+++ WebBlazor/Program.cs
@@ -1,8 +1,10 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Service.Interfaces;
 using Service.Services;
 using WebBlazor;
+using WebBlazor.Pages.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -11,5 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericService<>));
+builder.Services.AddScoped<FirebaseAuthService>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
