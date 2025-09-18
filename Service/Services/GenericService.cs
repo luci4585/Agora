8f5651b44dc2c312a90bf8b6e24772c74e98757e
luci4585 <lucilencina6@gmail.com>
2025-09-18 18:16:54 -0300

    Actualiza configuración de API y mejora autenticación
    Se ha restaurado la configuración de `_endpoint` en `GenericService` para utilizar `Properties.Resources.urlApi`.
    
    En `NavMenu.razor`, se ha ajustado el texto del enlace de navegación para reflejar el estado de autenticación del usuario y se ha añadido un manejador de eventos `LoginHasChanged` para actualizar la interfaz de usuario cuando el estado de autenticación cambia.

--- Service/Services/GenericService.cs
+++ Service/Services/GenericService.cs
@@ -21,8 +21,8 @@ namespace Service.Services
         {
             _httpClient = new HttpClient();
             _options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-            //_endpoint = Properties.Resources.urlApi + ApiEndpoints.GetEndpoint(typeof(T).Name);
-            _endpoint = Properties.Resources.UrlApiLocal+ApiEndpoints.GetEndpoint(typeof(T).Name);
+            _endpoint = Properties.Resources.urlApi + ApiEndpoints.GetEndpoint(typeof(T).Name);
+            //_endpoint = Properties.Resources.UrlApiLocal+ApiEndpoints.GetEndpoint(typeof(T).Name);
         }
 
         public async Task<List<T>?> GetAllAsync(string? filtro="")
